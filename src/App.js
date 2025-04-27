d6648ccc974b497ed31d9d763fe69b64e157bc86
urvsh27 <myappradar@gmail.com>
2025-04-28 01:41:17 +0530

    wss changes

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ const App = () => {
     "sk_2df18c6501727bf909af532bf3519dfe98eb504ed57106a2";
   const ELEVEN_LABS_VOICE_ID = "JBFqnCBsd6RMkjVDRZzb";
   // const BACKEND_WEB_SOCKET = "ws://80.225.223.153/api/";
-  const BACKEND_WEB_SOCKET = "wss://80.225.223.153/api";
+  const BACKEND_WEB_SOCKET = "wss://80.225.223.153/api/";
   const BACKEND_SERVER_URL = "https://80.225.223.153/api/";
 
   useEffect(() => {
